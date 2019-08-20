12f2f2cd978b37d334f77960616804c7c922dca0
raulvenson <raulvenson@gmail.com>
2019-08-20 14:02:28 -0400

    tornando header.js um function component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import FoodPortoImg from './../../assets/foodPortoImg.jpg'
 import {Row} from 'react-bootstrap';
 import Container from '@material-ui/core/Container';
@@ -8,18 +8,16 @@ import Container from '@material-ui/core/Container';
 
 import './header.css'
 
-class Header extends Component {	
-	render() {
-		return(
-			<div className="header">
-				
-				<Container >
-					<Row>
-						<img alt="Logo do Food Park Porto" src={FoodPortoImg} />
-					</Row>
-				</Container>
-			</div>
-		)
-	}
+export default props => {	
+	return(
+		<div className="header">
+			
+			<Container >
+				<Row>
+					<img alt="Logo do Food Park Porto" src={FoodPortoImg} />
+				</Row>
+			</Container>
+		</div>
+	)
 }
-export default Header
+
