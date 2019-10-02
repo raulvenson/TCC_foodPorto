97f02af0838d963d3cb1f12b44e8838d19444891
raulvenson <raulvenson@gmail.com>
2019-10-02 11:24:06 -0400

    padronizado cardápios sem cadastro

--- src/components/Cardapios/TioLuigi.js
+++ src/components/Cardapios/TioLuigi.js
@@ -13,7 +13,7 @@ import Modal from '../Modal/Modal'
 import AvaliacaoModal from '../Modal/AvaliacaoModal'
 
 import './TheDogFather.css'
-import TDFicon from './../../assets/thedog.png'
+import SemImagem from './../../assets/semImagem.jpg'
 
 import Grid from '@material-ui/core/Grid';
 
@@ -80,8 +80,8 @@ export default function CustomizedExpansionPanels() {
       <div className="header">
       <Paper className={classes.root}>
         <div className="font-lista cabecalho">
-          <img src={TDFicon}  className="img-header" alt=" The Dog Father icon"/>
-          Fino Sabor
+          <img src={SemImagem}  className="img-header" alt="General Mex logo"/>
+          General Mex
         </div>    
         <div className="cabecalho icones">
           <Link to="/listaRestaurantes" style={{ textDecoration: 'none' }}><TiArrowBackOutline className="back-icon"/></Link>
@@ -94,27 +94,39 @@ export default function CustomizedExpansionPanels() {
       <div className="content">
       <ExpansionPanel className="paper-root" square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <ExpansionPanelSummary aria-controls="panel1d-content" id="panel1d-header">
-          <div className="font-topic">Frango a Passarinho</div>
+          <div className="font-topic">Entradas</div>
         </ExpansionPanelSummary>
         
         <ExpansionPanelDetails className="details">
           <Typography>
             <Paper className={classes.root}>            
-              <Modal 
-                title="Porção Média 500g"
-                desc="Acompanhamento: polenta ou batata frita - molho barbecue e molho de alho"
-                price="A partir de R$30,00"
-                img="semImagem.jpg"
-                path="cardapioFS" 
+            <Modal 
+              title="Fritas TDF com Bacon e Cheddar"
+              desc="Porção de 400g de batatas fritas sequinhas com bacon e muito cheddar."
+              price="A partir de R$18,90"
+              img="semImagem.jpg"
+              path="cardapioGM" 
+              
               /> 
             </Paper>
             <Paper className={classes.root}>            
-              <Modal 
-                title="Porção Média 1kg"
-                desc="Acompanhamento: polenta ou batata frita - molho barbecue e molho de alho"
-                price="A partir de R$45,00"
-                img="semImagem.jpg"
-                path="cardapioFS" 
+            <Modal 
+              title="Mix TDF"
+              desc="200g de onion rings, 200g de mussarela sticks e 300g de mini salsichas artesanais berna, acompanhadas de cheddar, barbecue e mostarda amarela."
+              price="A partir de R$38,90"
+              img="semImagem.jpg"
+              path="cardapioGM" 
+              
+              /> 
+            </Paper>
+            <Paper className={classes.root}>            
+            <Modal 
+              title="Porção de Salsicha Artesanal"
+              desc="Salsicha suína e bovina artesanal berna, acompanhada de pão alemão, barbecue e mostarda amarela."
+              price="A partir de R$19,90"
+              img="semImagem.jpg"
+              path="cardapioGM" 
+              
               /> 
             </Paper>
           </Typography>
@@ -122,7 +134,7 @@ export default function CustomizedExpansionPanels() {
       </ExpansionPanel>
       <ExpansionPanel className="paper-root" square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <ExpansionPanelSummary aria-controls="panel2d-content" id="panel2d-header">
-          <div className="font-topic">Tapiocas Salgadas</div>
+          <div className="font-topic">Hot Dogs Premium</div>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className="details">
           <Typography>
@@ -131,127 +143,11 @@ export default function CustomizedExpansionPanels() {
                 title="01 - Fredo"
                 desc="Salsicha TDF, milho e ervilha. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
                 price="A partir de R$14,90"
-                img="fredo.png"
-                path="cardapioTDF" 
+                img="semImagem.jpg"
+                path="cardapioGM" 
                 /> 
             </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="02 - Vito"
-                desc="Salsicha TDF, chilli e Doritos. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="vito.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="03 - Connie"
-                desc="Salsicha de frango, bacon e milho. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="connie.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="04 - Tom"
-                desc="Salsicha TDF, cheddar e cebola caramelizada. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="tom.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="05 - Luca "
-                desc=" Salsicha TDF, pepperoni, sour cream e vinagrete. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="luca.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="06 - Carmella"
-                desc="Salsicha TDF, tomate, manjericão, azeite e orégano. Acompanha pão, purê de batata, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="carmella.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="07 - Mike"
-                desc="Salsicha TDF, calabresa, sour cream, vinagrete e barbecue. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="mike.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="08 - Corleone"
-                desc="Salsicha TDF envelopada no bacon e cheddar. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$19,90"
-                img="corleone.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="09 - Salvatore"
-                desc="Salsicha de frango e frango grelhado em tiras ao molho cream cheese, mostarda e mel. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="salvatore.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="10 - Virgil"
-                desc="Salsicha vegetal, abobrinha grelhada em rodelas, pimentões e cebola roxa, refogados com molho oriental. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$18,90"
-                img="virgil.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="11 - Moe"
-                desc="Salsicha TDF, costelinha de porco ao molho barbecue e toque de maionese de limão. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$19,90"
-                img="moe.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="12 - Rocco"
-                desc="Salsicha TDF, chilli, bacon em cubos, cheddar e pimenta biquinho. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$23,90"
-                img="rocco.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="13 - Jack"
-                desc="Salsicha TDF, costela suína desfiada, barbecue, bacon em cubos, onion rings e maionese com limão. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$23,90"
-                img="jack.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="14 - Double Corleone"
-                desc="Salsicha TDF, muito bacon e dobro de cheddar. Acompanha pão, purê de batata, salsicha, molho especial, batata palha e queijo maçaricado."
-                price="A partir de R$23,90"
-                img="doubleCorleone.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
+           
             </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
@@ -267,8 +163,8 @@ export default function CustomizedExpansionPanels() {
                 title="Linguicinhas TDF"
                 desc="400g de linguicinha artesanal berna recheada de queijo provolone, acompanhada de pão alemão, chimichurri e mostarda escura."
                 price="R$36,90" 
-                img="linguicinha.png"
-                path="cardapioTDF" 
+                img="semImagem.jpg"
+                path="cardapioGM" 
                 /> 
             </Paper>
           </Typography>
@@ -285,35 +181,8 @@ export default function CustomizedExpansionPanels() {
                 title="Água mineral"
                 desc="500 ml."
                 price="R$4,90" 
-                img="agua.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="Refrigerantes "
-                desc="Lata (350 ml) - Coca-cola, Coca-cola Zero, Fanta Guaraná, Fanta Laranja, Fanta Uva, Sprite."
-                price="R$5,90" 
-                img="refri.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="Soda Italiana"
-                desc="Soda Italiana Monin (400 ml) - Maçã verde, Morango, Tangerina, Cramberry."
-                price="R$7,90" 
-                img="soda.png"
-                path="cardapioTDF" 
-                /> 
-            </Paper>
-            <Paper className={classes.root}>            
-              <Modal 
-                title="Sucos"
-                desc="Dell Valle (300 ml) - Laranja, Uva."
-                price="R$6,90" 
-                img="sucos.png"
-                path="cardapioTDF" 
+                img="semImagem.jpg"
+                path="cardapioGM" 
                 /> 
             </Paper>
           </Typography>
@@ -330,8 +199,8 @@ export default function CustomizedExpansionPanels() {
                 title="Häagen-Dazs"
                 desc="(100ml) - Cookies, Morango, Macadâmia, Doce de Leite, Chocolate Belga."
                 price="R$13,90" 
-                img="haagen.png"
-                path="cardapioTDF" 
+                img="semImagem.jpg"
+                path="cardapioGM" 
                 /> 
             </Paper>
           </Typography>
