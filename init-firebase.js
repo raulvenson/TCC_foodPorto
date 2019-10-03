1c0e81bfc854d5f7e69bb9c021b597630844536f
raulvenson <raulvenson@gmail.com>
2019-10-03 14:31:10 -0400

    adicionado configuração e keys para o firestore

--- init-firebase.js
+++ init-firebase.js
@@ -1,21 +1,9 @@
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import * as firebase from "firebase/app";
 
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+var admin = require("firebase-admin");
 
-const firebaseConfig = {
-    apiKey: "AIzaSyASD8iaxtiAPSSKV9xQ5vktAUC8npR54tQ",
-    authDomain: "foodportotcc.firebaseapp.com",
-    databaseURL: "https://foodportotcc.firebaseio.com",
-    projectId: "foodportotcc",
-    storageBucket: "foodportotcc.appspot.com",
-    messagingSenderId: "518604574406",
-    appId: "1:518604574406:web:ce292dbedfac5eed"
-  };
+var serviceAccount = require("path/to/serviceAccountKey.json");
 
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://foodportotcc.firebaseio.com"
+});
